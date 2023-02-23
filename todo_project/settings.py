b40ba3bf7ee21fbc5b4aa530b318a65d496b7522
BOB13AM <bob.ibravic@gmail.com>
2023-02-23 12:34:04 +0700

    back to original

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+LANGUAGE_CODE = 'en'
 
 ROOT_URLCONF = 'todo_project.urls'
 
